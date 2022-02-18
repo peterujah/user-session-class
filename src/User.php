2b7795cb6fc5a364a889c3ad4d751f5da77ba8f0
Chigozie peter <ujahchigozie@gmail.com>
2022-02-18 21:00:23 +0800

    Create User.php

---
+++ src/User.php
@@ -0,0 +1,82 @@
+<?php 
+/**
+ * Cache - A simple php file cache
+ * @author      Peter Chigozie(NG) peterujah
+ * @copyright   Copyright (c), 2021 Peter(NG) peterujah
+ * @license     MIT public license
+ */
+namespace Peterujah\NanoBlock;
+use \Peterujah\NanoBlock\DBController;
+class User{
+    private $index = "login";
+	  protected $db;
+    protected $conn_handler;
+    protected $instanceQuery;
+	  public function __construct($db) {
+        $this->db = $db;
+        $this->setUserQuery("
+            SELECT *
+            FROM  users 
+            WHERE user_key = :check_user_key
+            LIMIT 1
+        ");
+	  }
+  
+    public function setUserQuery($query){
+      $this->instanceQuery = $query;
+    }
+  
+    public function add($index, $value){
+        $_SESSION[$this->db][$index] = $value;
+        return $this;
+    }
+
+    public function id(){
+        return $this->get("id");
+    }
+
+    public function online(){
+        return (isset($_SESSION[$this->db]) && !empty($_SESSION[$this->db]));
+    }
+
+    public function forceAuthLogin(){
+        if(!$this->online()){
+            header("Location: {$this->index}"); exit();
+        }
+    }
+
+    public function arrayData(){
+        return $_SESSION[$this->db]??[];
+    }
+
+    public function get($index){
+        return $_SESSION[$this->db][$index]??null;
+    }
+
+    public function remove($index){
+        unset($_SESSION[$this->db][$index]);
+        return $this;
+    }
+
+    public function clear(){
+        unset($_SESSION[$this->db]);
+        return $this;
+    }
+
+    public function conn(){
+        if(empty($this->conn_handler)){
+            $this->conn_handler = new Conn($_SERVER["SERVER_NAME"]=="localhost");
+        }
+        return $this->conn_handler;
+    }
+
+    public function instance(){
+        $this->conn()->prepare($this->instanceQuery);
+        $this->conn()->bind(":check_user_key", $this->get("key"));
+        $this->conn()->execute();		
+        $user = $this->conn()->getOne();
+        $this->conn()->free();
+        return $user;
+    }
+}
+
